7708408c907e5db2a2cdd4e4fb24697371e6e5d9
talgat55 <lightx_design@yahoo.com>
2021-05-24 00:20:12 +0600

    add styles for block sort and add click event for  item

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,34 +1,68 @@
-import React, {memo} from 'react';
-import FilterItem from './FilterItem';
+import React, {memo } from 'react';
+import SortItem from './SortItem';
+import styled from 'styled-components';
+
 interface interfaceRecommendation {
     slug: string,
     name: string,
 }
+
 const Filter: React.FC = () => {
-    const RECOMMENDATIONS_LIST : Array<interfaceRecommendation> = [
+    const RECOMMENDATIONS_LIST: Array<interfaceRecommendation> = [
         {
             slug: "recommendation",
             name: "Мы рекомендуем",
-        },{
+        }, {
             slug: "new",
             name: "Новинки",
-        },{
+        }, {
             slug: "price_desc",
             name: "Цена по убыванию",
-        },{
+        }, {
             slug: "price_asc",
             name: "Цена по возрастанию",
         }
     ]
 
+
+
     return (
-        <div>
-            <FilterItem
+        <FilterComponent>
+            <SortItem
                 title="Сортровка"
                 items={RECOMMENDATIONS_LIST}
             />
-        </div>
+        </FilterComponent>
     )
 }
 
 export default memo(Filter);
+
+const FilterComponent = styled.div`
+  .filter_sort-title{
+    font-size: 15px;
+    color: #d7d7d7;
+  }
+  
+  .filter_sort-items{
+    margin: 0;
+    padding: 0;
+    display: flex;
+    
+    li {
+      list-style: none;
+      margin-right: 10px;
+      border-bottom: 1px dashed #333; 
+      cursor: pointer;
+      user-select: none;
+      transition: all .3s;
+      &:hover{
+        color: #B4F8C8;
+      }
+      &.active{
+        color: #529d6f;
+      }
+    }
+  }
+
+`;
---
+++ src/components/SortItem.tsx
@@ -0,0 +1,43 @@
+import React, {memo, useState} from 'react';
+
+interface filterItemInterface {
+    title?: string,
+    items?: any
+}
+
+interface interfaceRecommendation {
+    slug: string,
+    name: string,
+}
+
+const SortItem: React.FC<filterItemInterface> = ({
+                                                     title,
+                                                     items
+                                                 }) => {
+    const [activeElement , setActiveElement] = useState(null);
+    const handlerClickOnSortItem = (index: number) => {
+        setActiveElement(index);
+    }
+    return (
+        <>
+
+            <h3 className="filter_sort-title">
+                {title}:
+            </h3>
+            <ul className="filter_sort-items">
+                {
+                    items.map((item, index) => (
+                        <li
+                            className={activeElement === index ? 'active' : '' }
+                            key={index}
+                            onClick={() => handlerClickOnSortItem(index)}
+                        >
+                            {item.name}
+                        </li>
+                    ))
+                }
+            </ul>
+        </>
+    )
+}
+export default memo(SortItem);
